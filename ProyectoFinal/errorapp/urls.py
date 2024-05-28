bda106b51cc942bd463fb621a4b7bb46fcaf2fae
jrueger71 <jc.rueger@gmail.com>
2024-05-28 14:07:11 -0400

    Prueba commit

--- ProyectoFinal/errorapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('404/', views.error_404, name='error_404'),
-]
--- ProyectoFinal/errorapp/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def error_404(request, exception):
-    return render(request, 'errorapp/404.html', status=404)
